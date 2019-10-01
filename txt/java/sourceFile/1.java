ea59f233331ee56cdf52c30a5391abb3356bef57
Kim SangJae <tkdwo287@gmail.com>
2019-10-01 12:42:01 +0900

    Add Language Source Files

---
+++ txt/java/sourceFile/1.java
@@ -0,0 +1,22 @@
+package _1번;
+
+public class 실습4_1번 {
+	public static void main(String[] args) {
+			TV myTV = new TV("LG",2017,32);
+			myTV.show();
+	}
+}
+
+class TV {
+	String manufacturer;
+	int year, inch;
+	public TV(String manufacturer, int year, int inch ){
+		this.manufacturer = manufacturer;
+		this.year = year;
+		this.inch = inch;
+	}
+	
+	public void show() {
+		System.out.println(manufacturer+"에서 만든 "+year+"년형 "+inch+"인치 TV");
+	}
+}
---
+++ txt/java/sourceFile/3.java
@@ -0,0 +1,29 @@
+package _3번;
+
+public class 실습4_3 {
+	public static void main(String[] args) {
+		Song s = new Song("Dancing Queen","ABBA", "스웨덴", 1978);
+		s.show();
+	}
+}
+
+class Song{
+	private String title, artist, country;
+	private int year;
+	public Song()
+	{
+		this("","","",0);
+	}
+	public Song(String title, String artist, String country, int year)
+	{
+		this.title = title;
+		this.artist = artist;
+		this.country = country;
+		this.year = year;
+	}
+	
+	public void show()
+	{
+		System.out.println(year+"년 "+country+"국적의 "+artist+"가 부른 "+title);
+	}
+}
